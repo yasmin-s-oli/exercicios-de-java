d0395eee96b766c651e0f3fad5ec54b53f51b13a
yasmin-s-oli <yasmin.oliveira1@aluno.ifsp.edu.br>
2025-03-03 19:59:42 -0300

    Add files via upload

---
+++ ex1.js
@@ -0,0 +1,7 @@
+let idade = 16
+let nome = 'yasmin'
+let boleano = true
+
+console.log(typeof idade)
+console.log(typeof nome)
+console.log(typeof boleano)
---
+++ ex2.js
@@ -0,0 +1,15 @@
+let nota1 = 10
+let nota2 = 4
+let nota3 = 4
+
+
+let média = (nota1+nota2+nota3)/3
+console.log(`A sua média é ${média}`)
+
+if (média >=7){
+    console.log(`você esta aprovado`)
+}
+else {
+    console.log(`você foi reprovado`)
+    
+}
